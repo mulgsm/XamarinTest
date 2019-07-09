c5a08991c1dc63425c132c2e785ee187ee2424de
Müller Miranda <mullergsm@hotmail.com>
2019-07-09 14:26:05 -0300

    Commit feature

--- XamarinTest/XamarinTest/Views/MainPage.xaml.cs
+++ XamarinTest/XamarinTest/Views/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using Xamarin.Forms.Xaml;
 namespace XamarinTest.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : MasterDetailPage {
+
+
         Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
 
 
