ddf448f3f202a585e5f765092465c4414c42cddf
Müller Miranda <mullergsm@hotmail.com>
2019-07-09 14:25:31 -0300

    Commit Feature Issue-003

--- XamarinTest/XamarinTest/Views/MainPage.xaml.cs
+++ XamarinTest/XamarinTest/Views/MainPage.xaml.cs
@@ -9,6 +9,8 @@ namespace XamarinTest.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : MasterDetailPage {
         Dictionary<int, NavigationPage> MenuPages = new Dictionary<int, NavigationPage>();
+
+
         public MainPage() {
             InitializeComponent();
             MasterBehavior = MasterBehavior.Popover;
